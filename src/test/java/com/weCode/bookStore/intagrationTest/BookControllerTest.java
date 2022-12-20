b4c867b385dba33ff400ad6c1f2034cfd09bdac0
MariiaS3 <mariia.sydor28082001@gmail.com>
2022-12-20 17:52:19 +0100

    integration test

---
+++ src/test/java/com/weCode/bookStore/intagrationTest/BookControllerTest.java
@@ -0,0 +1,34 @@
+package com.weCode.bookStore.intagrationTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.jdbc.Sql;
+
+import com.weCode.bookStore.BookStoreApplication;
+import com.weCode.bookStore.dto.BookDto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = BookStoreApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class BookControllerTest {
+    
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    @Sql(scripts = {"classpath:InsertInitialBookRecordForTest.sql"})
+    void shouldRetornBooksWhenGetBookApiColled(){
+        BookDto[] listOfBooks = testRestTemplate.getForObject("http://localhost:"+port+"/api/v1/books", BookDto[].class);
+        assertThat(listOfBooks).isNotNull();
+        assertThat(listOfBooks.length).isEqualTo(2);
+    }
+
+}
