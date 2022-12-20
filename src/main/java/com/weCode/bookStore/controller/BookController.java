9a7d6c9c346fcb546fbb419ae8be3befa3b6e1d0
MariiaS3 <mariia.sydor28082001@gmail.com>
2022-12-20 16:54:13 +0100

    circleci

--- src/main/java/com/weCode/bookStore/controller/BookController.java
+++ src/main/java/com/weCode/bookStore/controller/BookController.java
@@ -3,7 +3,6 @@ package com.weCode.bookStore.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
