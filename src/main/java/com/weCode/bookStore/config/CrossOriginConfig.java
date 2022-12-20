a13eebeb128a429356ae58628a7f4f9ba2a231e5
MariiaS3 <mariia.sydor28082001@gmail.com>
2022-12-20 22:50:39 +0100

    add cros mapping

--- src/main/java/com/weCode/bookStore/config/CrossOriginConfig.java
+++ src/main/java/com/weCode/bookStore/config/CrossOriginConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class CrossOriginConfig implements WebMvcConfigurer{
        
-       @Override
+    //    @Override
        public void addCorsMappings(CorsRegistry registry) {
            registry.addMapping("/**");
        }
