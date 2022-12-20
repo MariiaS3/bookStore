2f971fc508403703b441c3cc54f9ba99f50bc962
MariiaS3 <mariia.sydor28082001@gmail.com>
2022-12-20 23:02:08 +0100

    add cros mapping

--- src/main/java/com/weCode/bookStore/config/CrossOriginConfig.java
+++ src/main/java/com/weCode/bookStore/config/CrossOriginConfig.java
@@ -1,10 +1,11 @@
 package com.weCode.bookStore.config;
 
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
+@Configuration
 public class CrossOriginConfig implements WebMvcConfigurer{
        
     //    @Override
